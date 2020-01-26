74e5c250aabf86230e10a9df7751c0d583efd70f
Matin Ostovan <matinostovan@gmail.com>
2020-01-25 17:44:09 -0800

    Test Board PID Code

--- 2020 Infinite Recharge/src/main/java/frc/Mechanisms/CatzDriveTrain.java
+++ 2020 Infinite Recharge/src/main/java/frc/Mechanisms/CatzDriveTrain.java
@@ -1,10 +1,13 @@
 package frc.Mechanisms;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -46,57 +49,120 @@ public class CatzDriveTrain
 
     private final int DRVTRAIN_LGEAR_SOLENOID_PCM_PORT_A = 0;
     private final int DRVTRAIN_HGEAR_SOLENOID_PCM_PORT_B = 1;
+    public double kP, kI, kD, kF;
 
     public CatzDriveTrain() 
     {
         drvTrainMtrCtrlLTFrnt = new WPI_TalonFX(DRVTRAIN_LT_FRNT_MC_CAN_ID);
-        drvTrainMtrCtrlLTBack = new WPI_TalonFX(DRVTRAIN_LT_BACK_MC_CAN_ID);
+        //drvTrainMtrCtrlLTBack = new WPI_TalonFX(DRVTRAIN_LT_BACK_MC_CAN_ID);
 
-        //drvTrainMtrCtrlLTBack.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 100);
+        drvTrainMtrCtrlLTFrnt.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 100);
         //0 = primary loop
         //100 = timeout in ms
+       
+    
+     
+        
+        drvTrainMtrCtrlLTFrnt.config_kP(0, 0.04);
+        drvTrainMtrCtrlLTFrnt.config_kI(0, 0.0);
+        drvTrainMtrCtrlLTFrnt.config_kD(0, 1);
+        drvTrainMtrCtrlLTFrnt.config_kF(0, 1);
+        drvTrainMtrCtrlLTFrnt.config_IntegralZone(0, 0);
 
+        SmartDashboard.putNumber("P Gain", kP);
+
+        SmartDashboard.putNumber("I Gain", kI);
+    
+        SmartDashboard.putNumber("D Gain", kD);
+    
+        SmartDashboard.putNumber("Feed Forward", kF);
+
+        
+
+           
+        
+        
         drvTrainMtrCtrlRTFrnt = new WPI_TalonFX(DRVTRAIN_RT_FRNT_MC_CAN_ID);
-        drvTrainMtrCtrlRTBack = new WPI_TalonFX(DRVTRAIN_RT_BACK_MC_CAN_ID);
+        //drvTrainMtrCtrlRTBack = new WPI_TalonFX(DRVTRAIN_RT_BACK_MC_CAN_ID);
 
         drvTrainMtrCtrlLTFrnt.setNeutralMode(NeutralMode.Coast);
-        drvTrainMtrCtrlLTBack.setNeutralMode(NeutralMode.Coast);
+        //drvTrainMtrCtrlLTBack.setNeutralMode(NeutralMode.Coast);
         drvTrainMtrCtrlRTFrnt.setNeutralMode(NeutralMode.Coast);
-        drvTrainMtrCtrlRTBack.setNeutralMode(NeutralMode.Coast);
-
-        /*
-        currentLimitConfigArray[enabledStatusIndex] = enabled;
+        //drvTrainMtrCtrlRTBack.setNeutralMode(NeutralMode.Coast);
+        
+        /*currentLimitConfigArray[enabledStatusIndex] = enabled;
         currentLimitConfigArray[currentLimitIndex] = currentLimit;
         currentLimitConfigArray[triggerThresholdCurrentIndex] = triggerThresholdCurrent;
         currentLimitConfigArray[triggerThresholdTimeIndex] = triggerThresholdTime; 
 
-        StatorCurrentLimitConfiguration s = new StatorCurrentLimitConfiguration(currentLimitConfigArray);
+        StatorCurrentLimitConfiguration s = new StatorCurrentLimitConfiguration(currentLimitConfigArray);*/
 
-        drvTrainMtrCtrlLTFrnt.configGetStatorCurrentLimit(s);
-        drvTrainMtrCtrlLTBack.configGetStatorCurrentLimit(s);
-        drvTrainMtrCtrlRTFrnt.configGetStatorCurrentLimit(s);
-        drvTrainMtrCtrlRTBack.configGetStatorCurrentLimit(s);*/
+        //drvTrainMtrCtrlLTFrnt.configGetStatorCurrentLimit(s);
+        //drvTrainMtrCtrlLTBack.configGetStatorCurrentLimit(s);
+        //drvTrainMtrCtrlRTFrnt.configGetStatorCurrentLimit(s);
+        //drvTrainMtrCtrlRTBack.configGetStatorCurrentLimit(s);
 
-        drvTrainLT = new SpeedControllerGroup(drvTrainMtrCtrlLTFrnt, drvTrainMtrCtrlLTBack);
-        drvTrainRT = new SpeedControllerGroup(drvTrainMtrCtrlRTFrnt, drvTrainMtrCtrlRTBack);
+        drvTrainLT = new SpeedControllerGroup(drvTrainMtrCtrlLTFrnt);//, drvTrainMtrCtrlLTBack);
+        drvTrainRT = new SpeedControllerGroup(drvTrainMtrCtrlRTFrnt);//, drvTrainMtrCtrlRTBack);
 
-        drvTrainDifferentialDrive = new DifferentialDrive(drvTrainLT, drvTrainRT);
+        //drvTrainDifferentialDrive = new DifferentialDrive(drvTrainLT, drvTrainRT);
 
         gearShifter = new DoubleSolenoid(DRVTRAIN_LGEAR_SOLENOID_PCM_PORT_A, DRVTRAIN_HGEAR_SOLENOID_PCM_PORT_B);
     }
 
-    public void arcadeDrive(double power, double rotation)
-    {
+    public void arcadeDrive(final double power, final double rotation) {
         drvTrainDifferentialDrive.arcadeDrive(power, rotation);
     }
 
-    public void deployGearShift()
-    {
+    public void setDriveKp(int slotIdx, double kpValue) {
+        drvTrainMtrCtrlLTFrnt.config_kP(slotIdx, kpValue);
+    }
+    public void setDriveKI(int slotIdx, double kiValue) {
+        drvTrainMtrCtrlLTFrnt.config_kI(slotIdx, kiValue);
+    }
+    public void setDriveKD(int slotIdx, double kdValue) {
+        drvTrainMtrCtrlLTFrnt.config_kD(slotIdx, kdValue);
+    }
+    public void setDriveKF(int slotIdx, double kfValue) {
+        drvTrainMtrCtrlLTFrnt.config_kF(slotIdx, kfValue);
+    }
+
+
+    public void deployGearShift() {
         gearShifter.set(Value.kReverse);
     }
-    
-    public void retractGearShift()
-    {
+
+    public void retractGearShift() {
         gearShifter.set(Value.kForward);
+    }
+
+    public double getLTEncPosition() {
+        return drvTrainMtrCtrlLTFrnt.getSensorCollection().getIntegratedSensorPosition();
+
+    }
+
+    public double getLTEncVelocity() {
+        return drvTrainMtrCtrlLTFrnt.getSensorCollection().getIntegratedSensorVelocity();
+    }
+
+    public double getRTEncPostion() {
+        return drvTrainMtrCtrlRTFrnt.getSelectedSensorPosition(0);
+    }
+
+    public double getRTEncVelocity() {
+        return drvTrainMtrCtrlRTFrnt.getSelectedSensorVelocity(0);
+    }
+
+    public void setTargetPosition(final double targetPosition) {
+        drvTrainMtrCtrlLTFrnt.set(TalonFXControlMode.Position, targetPosition);
+    }
+
+    public void setTargetVelocity(final double targetVelocity) {
+        drvTrainMtrCtrlLTFrnt.set(TalonFXControlMode.Velocity, targetVelocity);
+    }
+
+    public void setEncPosition(final int position)
+    {
+        drvTrainMtrCtrlLTFrnt.setSelectedSensorPosition(position);
     }
 }    
--- 2020 Infinite Recharge/src/main/java/frc/robot/Robot.java
+++ 2020 Infinite Recharge/src/main/java/frc/robot/Robot.java
@@ -6,8 +6,11 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.Mechanisms.CatzDriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -16,37 +19,114 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends TimedRobot 
+{
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
+  CatzDriveTrain driveTrain;
+
+  XboxController xboxDrv;
+
   @Override
-  public void robotInit() {
+  public void robotInit() 
+  {
+    driveTrain = new CatzDriveTrain();
+
+    xboxDrv = new XboxController(0);
   }
 
   @Override
-  public void autonomousInit() {
+  public void robotPeriodic() 
+  {
+    SmartDashboard.putNumber("Enc Pos", driveTrain.getLTEncPosition());
+    SmartDashboard.putNumber("Enc Vel", driveTrain.getLTEncVelocity());
+
+
   }
 
   @Override
-  public void autonomousPeriodic() {
+  public void autonomousInit() 
+  {
   }
 
   @Override
-  public void teleopInit() {
+  public void autonomousPeriodic() 
+  {
   }
 
   @Override
-  public void teleopPeriodic() {
+  public void teleopInit() 
+  {
+
   }
 
   @Override
-  public void testInit() {
+  public void teleopPeriodic() 
+  {
+    if(xboxDrv.getBumper(Hand.kRight))
+    //{
+      //driveTrain.arcadeDrive(xboxDrv.getY(Hand.kLeft), xboxDrv.getX(Hand.kRight));
+    //}
+    if(xboxDrv.getAButtonPressed())
+    {
+      driveTrain.setTargetVelocity(0);
+    }
+    else if(xboxDrv.getBButtonPressed())
+    {
+      driveTrain.setTargetVelocity(10000);
+    }
+    else if(xboxDrv.getXButtonPressed())
+    {
+      driveTrain.setTargetVelocity(500);
+    }
+    else if(xboxDrv.getYButtonPressed())
+    {
+      driveTrain.setEncPosition(0);
+    }
+   // read PID coefficients from SmartDashboard
+        
+   final double p = SmartDashboard.getNumber("P Gain", 0);
+        
+   final double i = SmartDashboard.getNumber("I Gain", 0);
+
+   final double d = SmartDashboard.getNumber("D Gain", 0);
+
+   
+
+   final double f = SmartDashboard.getNumber("Feed Forward", 0);
+
+  
+
+
+
+   // if PID coefficients on SmartDashboard have changed, write new values to controller
+
+   if((p != driveTrain.kP)) {driveTrain.setDriveKp(0,p); driveTrain.kP = p; }
+
+   if((i != driveTrain.kI)) { driveTrain.setDriveKI(0,i); driveTrain.kI = i; }
+
+   if((d != driveTrain.kD)) { driveTrain.setDriveKD(0,d); driveTrain.kD = d; }
+
+
+   if((f != driveTrain.kF)) { driveTrain.setDriveKF(0,f); driveTrain.kF = f; }
+
+
+
+   }
+  
+
+  @Override
+  public void testInit() 
+  {
+
   }
 
   @Override
-  public void testPeriodic() {
+  public void testPeriodic() 
+  {
+
   }
 
 }
