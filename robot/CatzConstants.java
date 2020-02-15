5bd3469e870fb214e1e21e22cbc000800c520389
yutayogi19 <60044718+yutayogi19@users.noreply.github.com>
2020-02-15 15:36:44 -0800

    CatzConstants

---
+++ robot/CatzConstants.java
@@ -0,0 +1,52 @@
+package frc.robot;
+
+
+
+public class CatzConstants
+{
+
+    public static final int NAVX_RESET_WAIT_TIME = 1;
+    public static final int PCM_CANBUS_ID = 1;
+
+
+    public static final double ROBOT_LENGTH = 0; //TBD
+    public static final double ROBOT_WIDTH = 0; //
+    public static final double HALF_ROBOT_LENGTH = ROBOT_LENGTH/2;
+    public static final double HALF_ROBOT_WIDTH = ROBOT_WIDTH/2;
+
+    public static boolean USING_COMPETITION_ROBOT = true;
+
+    public static final double CONTROLLER_INPUT_WAIT_TIME = 0.005;
+
+    public static final double INVALID_ANGLE = 999.9; 
+    public static final double INVALID_EXT =   999.9;
+
+    public static final boolean USING_SOFT_LIMITS = false;
+
+    public static final double NEO_MOTOR_TEMP_LIMIT = 65;
+    public static final double NEO550_MOTOR_TEMP_LIMIT = 65;
+    public static final double FALCON_MOTOR_TEMP_LIMIT = 65;
+    public static final double PRO775_MOTOR_TEMP_LIMIT = 65;
+
+
+    public static final int SPARK_MAX_FIRMWARE = 0;
+
+    public static final int CANBUS_ISSUES = 0;
+    public static final int PDP_ISSUES = 1;
+    public static final int MOTOR_ISSUES = 2;
+
+    public static final int DRV_TRAIN_LT_FRNT = 0;
+    public static final int DRV_TRAIN_LT_BACK = 1;
+    public static final int CLIMB_A = 2;
+    public static final int CLIMB_B = 3;
+    public static final int INTAKE_LT = 4;
+    public static final int LIGHTSABER = 5;
+    public static final int INDEXER = 10 ;   
+    public static final int INTAKE_RT = 11;
+    public static final int SHOOT_B = 12;
+    public static final int SHOOT_A = 13;
+    public static final int DRV_TRAIN_RT_FRNT = 14;
+    public static final int DRV_TRAIN_RT_BACK = 15;
+
+
+}
