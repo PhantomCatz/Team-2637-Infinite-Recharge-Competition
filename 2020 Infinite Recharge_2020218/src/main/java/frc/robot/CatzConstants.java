1cacdd071dd5c06872ce80ba26f93e8a5d388636
Nicholas <njminutel@gmail.com>
2020-02-26 18:18:49 -0800

    Latest indexer code
    2/26/20

--- 2020 Infinite Recharge_2020218/src/main/java/frc/robot/CatzConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-public class CatzConstants
-{
-    public static final double ROBOT_LENGTH = 39; 
-    public static final double ROBOT_WIDTH = 35;
-    public static final double HALF_ROBOT_LENGTH = ROBOT_LENGTH/2;
-    public static final double HALF_ROBOT_WIDTH = ROBOT_WIDTH/2;
-}
